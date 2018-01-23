f3695c7cfa4270ee98b95da6ed7ec1174b7535e5
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-23 18:50:07 +0700

    chore: meta tweaks

--- __tests__/error.test.js
+++ __tests__/error.test.js
@@ -5,7 +5,7 @@ const { homedir } = require('os');
 const gStatus = require('../');
 
 describe('error', () => {
-  it('throws if if `cwd` is not a git repository', async () => {
+  it('throws if `cwd` is not a git repository', async () => {
     await expect(gStatus({ cwd: homedir() })).rejects.toThrowError(
       'fatal: Not a git repository (or any of the parent directories): .git'
     );
