9dfdf4c4ca7783d5bf2f9dddad121b6e53d49bbe
Lufty Wiranda <lufty.wiranda@gmail.com>
2018-01-23 16:11:08 +0700

    test(filterer): add tests

--- filterer.js
+++ filterer.js
@@ -2,9 +2,9 @@
 
 const matcher = require('matcher');
 
-module.exports = (obj, patterns, prop) => {
-  const status = obj.map(x => x[prop]);
+module.exports = (data, patterns, prop) => {
+  const status = data.map(x => x[prop]);
   const matched = matcher(status, Array.from(patterns));
 
-  return obj.filter(x => matched.includes(x[prop]));
+  return data.filter(x => matched.includes(x[prop]));
 };
